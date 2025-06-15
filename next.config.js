dc221c88d8d2494d54bbc91af71966373b8328d5
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2025-06-15 11:34:15 +0530

    deploy with errors

--- next.config.js
+++ next.config.js
@@ -1,16 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'zqwkdm0dqllxqofj.public.blob.vercel-storage.com',
-            port: '',
-            pathname: '/categories/**',
-          },
-        ],
-      },
-    reactStrictMode: true,
+  images: {
+      remotePatterns: [
+        // Existing pattern
+        {
+          protocol: 'https',
+          hostname: 'zqwkdm0dqllxqofj.public.blob.vercel-storage.com',
+          port: '',
+          pathname: '/categories/**',
+        },
+        
+        {
+          protocol: 'https',
+          hostname: 'imagesm.plexussquare.in',
+          port: '',
+          pathname: '**', 
+        },
+      ],
+    },
+  reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
